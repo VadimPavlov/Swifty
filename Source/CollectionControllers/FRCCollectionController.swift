867147564a983e77f9c6b7012a78765484484d66
Vadym Pavlov <vadim.pavlov@dataart.com>
2017-06-14 19:18:56 +0300

    obsering request predicate

--- Source/CollectionControllers/FRCCollectionController.swift
+++ Source/CollectionControllers/FRCCollectionController.swift
@@ -7,18 +7,33 @@ import CoreData
 
 public class FRCCollectionController<Object: NSFetchRequestResult>: CollectionController<Object>, NSFetchedResultsControllerDelegate {
     
-    public init(collectionView: UICollectionView? = nil, frc: NSFetchedResultsController<Object>, cellDescriptor: @escaping (Object) -> CellDescriptor) {
+    private let frc: NSFetchedResultsController<Object>
+    private let observingPredicate: Bool
+    
+    public init(collectionView: UICollectionView? = nil, frc: NSFetchedResultsController<Object>, observeRequestPredicate: Bool = true, cellDescriptor: @escaping (Object) -> CellDescriptor) {
+
+        self.frc = frc
+        self.observingPredicate = observeRequestPredicate
+
         let dataSource = DataSource(frc: frc)
         super.init(collectionView: collectionView, dataSource: dataSource, cellDescriptor: cellDescriptor)
         frc.delegate = self
+        
+        if observeRequestPredicate {
+            frc.fetchRequest.addObserver(self, forKeyPath: "predicate", options: .new, context: nil)
+        }
+    }
+    
+    deinit {
+        if observingPredicate {
+            frc.fetchRequest.removeObserver(self, forKeyPath: "predicate")
+        }
     }
     
     // MARK: - NSFetchedResultsControllerDelegate
-    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-    }
+    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {}
     
-    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-    }
+    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {}
     
     public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
         
@@ -31,13 +46,12 @@ public class FRCCollectionController<Object: NSFetchRequestResult>: CollectionCo
         BatchUpdate.perform(at: indexPath, for: type, newIndexPath: newIndexPath) {
             self.performBatch($0)
         }
-        
     }
 }
 
 public class SimpleFRCCollectionController<Object: NSFetchRequestResult, Cell: UICollectionViewCell>: FRCCollectionController<Object>  {
-    public init(collectionView: UICollectionView? = nil, frc: NSFetchedResultsController<Object>, identifier: String? = nil, register: CellDescriptor.Register? = nil, configure: @escaping (Cell, Object) -> Void) {
-        super.init(collectionView: collectionView, frc: frc) { object in
+    public init(collectionView: UICollectionView? = nil, frc: NSFetchedResultsController<Object>, observeRequestPredicate: Bool = true, identifier: String? = nil, register: CellDescriptor.Register? = nil, configure: @escaping (Cell, Object) -> Void) {
+        super.init(collectionView: collectionView, frc: frc, observeRequestPredicate: observeRequestPredicate) { object in
             let descriptor = CellDescriptor(identifier: identifier, register: register, configure: { cell in
                 configure(cell, object)
             })
--- Source/CollectionControllers/FRCTableController.swift
+++ Source/CollectionControllers/FRCTableController.swift
@@ -6,14 +6,31 @@ import UIKit
 import CoreData
 
 public class FRCTableController<Object: NSFetchRequestResult>: TableController<Object>, NSFetchedResultsControllerDelegate {
+    
+    private let frc: NSFetchedResultsController<Object>
+    private let observingPredicate: Bool
 
     public var animation: UITableViewRowAnimation = .automatic
-    
-    public init(tableView: UITableView? = nil, frc: NSFetchedResultsController<Object>, cellDescriptor: @escaping (Object) -> CellDescriptor) {
+
+    public init(tableView: UITableView? = nil, frc: NSFetchedResultsController<Object>, observeRequestPredicate: Bool = true, cellDescriptor: @escaping (Object) -> CellDescriptor) {
+        
+        self.frc = frc
+        self.observingPredicate = observeRequestPredicate
+        
         let dataSource = DataSource(frc: frc)
         super.init(tableView: tableView, dataSource: dataSource, cellDescriptor: cellDescriptor)
         frc.delegate = self
-    }    
+        
+        if observeRequestPredicate {
+            frc.fetchRequest.addObserver(self, forKeyPath: "predicate", options: .new, context: nil)
+        }
+    }
+    
+    deinit {
+        if observingPredicate {
+            frc.fetchRequest.removeObserver(self, forKeyPath: "predicate")
+        }
+    }
     
     // MARK: - NSFetchedResultsControllerDelegate
     public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
@@ -35,11 +52,18 @@ public class FRCTableController<Object: NSFetchRequestResult>: TableController<O
             self.performBatch($0, animation: self.animation)
         }
     }
+    
+    // MARK: - Observing
+    public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        DispatchQueue.main.async {
+            self.tableView?.reloadData()
+        }
+    }
 }
 
 public class SimpleFRCTableController<Object: NSFetchRequestResult, Cell: UITableViewCell>: FRCTableController<Object> {
-    public init(tableView: UITableView? = nil, frc: NSFetchedResultsController<Object>, identifier: String? = nil, register: CellDescriptor.Register? = nil, configure: @escaping (Cell, Object) -> Void) {
-        super.init(tableView: tableView, frc: frc) { object in
+    public init(tableView: UITableView? = nil, frc: NSFetchedResultsController<Object>, observeRequestPredicate: Bool = true, identifier: String? = nil, register: CellDescriptor.Register? = nil, configure: @escaping (Cell, Object) -> Void) {
+        super.init(tableView: tableView, frc: frc, observeRequestPredicate: observeRequestPredicate) { object in
             let descriptor = CellDescriptor(identifier: identifier, register: register, configure: { cell in
                 configure(cell, object)
             })
