2816bbdbcc7a8c9f696f88e00de8ed40faf2231b
Vadim Pavlov <vadim.pavlov@mobilestrategy.nl>
2017-10-25 14:37:30 +0300

    String HTML

--- Source/Foundation/String.swift
+++ Source/Foundation/String.swift
@@ -8,4 +8,12 @@ public extension String {
 	func foregroundColorString(color: UIColor) -> NSAttributedString {
 		return NSAttributedString(string: self, attributes: [.foregroundColor : color])
 	}
+    
+    func asHTML() -> NSAttributedString? {
+        guard let data = self.data(using: .utf8) else { return nil }
+        let attributed = try? NSAttributedString(data: data,
+                                            options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue],
+                                            documentAttributes: nil)
+        return attributed
+    }
 }
