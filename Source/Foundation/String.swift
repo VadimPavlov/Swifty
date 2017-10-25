b063fcc8ee0366637a36c5922f57a864f4fd53db
Vadim Pavlov <vadim.pavlov@mobilestrategy.nl>
2017-10-25 14:38:41 +0300

    formatiing

--- Source/Foundation/String.swift
+++ Source/Foundation/String.swift
@@ -11,8 +11,10 @@ public extension String {
     
     func asHTML() -> NSAttributedString? {
         guard let data = self.data(using: .utf8) else { return nil }
+        let type = NSAttributedString.DocumentType.html
+        let encoding = String.Encoding.utf8.rawValue
         let attributed = try? NSAttributedString(data: data,
-                                            options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue],
+                                            options: [.documentType: type, .characterEncoding: encoding],
                                             documentAttributes: nil)
         return attributed
     }
