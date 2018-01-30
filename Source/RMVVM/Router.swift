2cb387cf598d47958b576bda8d49f53796f14190
Vadim Pavlov <vadim.pavlov@mobilestrategy.nl>
2018-01-30 11:22:46 +0200

    RMVVM simplified

---
+++ Source/RMVVM/Router.swift
@@ -0,0 +1,32 @@
+//
+//  Router.swift
+//  Created by Vadim Pavlov on 2/22/17.
+
+import UIKit
+
+public protocol Router {
+    associatedtype Segue: RawRepresentable
+    func performSegue(_ segue: Segue, sender: Any?)
+    func prepareSegue(_ segue: Segue, destination: UIViewController, sender: Any?)
+}
+
+extension Router where Segue.RawValue == String, Self: UIViewController {
+
+    func performSegue(_ segue: Segue, sender: Any?) {
+        let identifier = segue.rawValue
+        if shouldPerformSegue(withIdentifier: identifier, sender: sender) {
+            performSegue(withIdentifier: identifier, sender: sender)
+        }
+    }
+
+    func prepare(storyboardSegue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = storyboardSegue.identifier else { return }
+        let destination = storyboardSegue.destination
+
+        if let segue = Segue(rawValue: identifier) {
+            self.prepareSegue(segue, destination: destination, sender: sender)
+        } else {
+            assertionFailure("Invalid identifier: \(identifier)")
+        }
+    }
+}
