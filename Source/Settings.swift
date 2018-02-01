6952d9d1feee77e4bc44e4e57ca7dafcb528463f
Vadim Pavlov <vadim.pavlov@mobilestrategy.nl>
2018-02-01 15:20:22 +0200

    Settings clearAll

--- Source/Settings.swift
+++ Source/Settings.swift
@@ -11,19 +11,28 @@ import Foundation
 public protocol SettingKey {
     init?(rawValue: String)
     var rawValue: String { get }
+    
+    static var allKeys: [Self] { get }
 }
 
 open class Settings<Key: SettingKey> {
+    
     fileprivate let defaults: UserDefaults
     public init(defaults: UserDefaults) {
         self.defaults = defaults
     }
-
+    
+    public func clearAll() {
+        Key.allKeys.forEach {
+            self.set(value: nil, forKey: $0)
+        }
+    }
+    
     public subscript<T>(key: Key) -> T? {
         set { self.set(value: newValue, forKey: key) }
         get { return self.get(key: key) as? T }
     }
-
+    
     public subscript<E: RawRepresentable>(key: Key) -> E? {
         set { self.set(value: newValue?.rawValue, forKey: key) }
         get {
@@ -34,7 +43,7 @@ open class Settings<Key: SettingKey> {
 }
 
 private extension Settings {
-    func set(value: Any?, forKey settingKey: SettingKey) {
+    func set(value: Any?, forKey settingKey: Key) {
         let key = settingKey.rawValue
         if let newValue = value {
             defaults.set(newValue, forKey: key)
@@ -42,8 +51,8 @@ private extension Settings {
             defaults.removeObject(forKey: key)
         }
     }
-
-    func get(key settingKey: SettingKey) -> Any? {
+    
+    func get(key settingKey: Key) -> Any? {
         let key = settingKey.rawValue
         return defaults.object(forKey: key)
     }
