7438efa5d32b937d505e8d2689d8a42d0ea4e512
Vadym Pavlov <vadim.pavlov@dataart.com>
2018-03-10 14:57:02 +0200

    Image from color

---
+++ Source/UIKit/UIImage.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage.swift
+//  Swifty
+//
+//  Created by Vadym Pavlov on 10.03.18.
+//  Copyright © 2018 Vadym Pavlov. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func image(with color: UIColor) -> UIImage {
+        let image = withRenderingMode(.alwaysTemplate)
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        color.set()
+
+        image.draw(in: CGRect(origin: .zero, size: size))
+        let result = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return result.withRenderingMode(.alwaysOriginal)
+    }
+}
+
