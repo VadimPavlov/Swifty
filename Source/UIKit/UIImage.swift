a40aec3f962b158f95f5e0c39b04558e9181b833
Vadym Pavlov <vadim.pavlov@dataart.com>
2018-03-10 15:01:48 +0200

    static color

--- Source/UIKit/UIImage.swift
+++ Source/UIKit/UIImage.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 public extension UIImage {
+
+    // MARK: - Colors
+    static func from(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+
     func image(with color: UIColor) -> UIImage {
         let image = withRenderingMode(.alwaysTemplate)
         UIGraphicsBeginImageContextWithOptions(size, false, scale)
