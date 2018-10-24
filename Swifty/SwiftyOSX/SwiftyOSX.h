6910f44391053a5ddaf0ce2c69eee2362d487993
Vadym Pavlov <vadim.pavlov@dataart.com>
2018-10-24 21:05:15 +0300

    osx support

---
+++ Swifty/SwiftyOSX/SwiftyOSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyOSX.h
+//  SwiftyOSX
+//
+//  Created by Vadym Pavlov on 10/24/18.
+//  Copyright © 2018 Vadym Pavlov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyOSX.
+FOUNDATION_EXPORT double SwiftyOSXVersionNumber;
+
+//! Project version string for SwiftyOSX.
+FOUNDATION_EXPORT const unsigned char SwiftyOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyOSX/PublicHeader.h>
+
+
