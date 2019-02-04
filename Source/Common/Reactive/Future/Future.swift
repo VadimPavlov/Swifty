da9e110a091c16b602c87ca56bfa1a14b800dc1e
Vadim Pavlov <vadim.pavlov@tbwamobile.com>
2019-02-04 23:43:07 +0200

    Future queue

--- Source/Common/Reactive/Future/Future.swift
+++ Source/Common/Reactive/Future/Future.swift
@@ -5,12 +5,18 @@
 import Foundation
 
 public struct Future<T> {
-    
+
     public typealias Completion = (Result<T>) -> Void
     public typealias Task = (@escaping Completion) -> Progress?
-    
+
     public let task: Task
-    
+
+    public enum CompletionQueue {
+        case result
+        case main
+        case current
+    }
+
     public init(_ task: @escaping Task) {
         self.task = task
     }
@@ -37,35 +43,46 @@ public extension Future where T == Void {
 }
 
 public extension Future {
-    
+
     @discardableResult
-    func start(completion: @escaping Completion) -> Progress? {
+    func start(queue: CompletionQueue = .result, completion: @escaping Completion) -> Progress? {
+
+        let main = OperationQueue.main
+        let current = OperationQueue.current ?? .main
+
         return self.task() { result in
-            completion(result)
+            switch queue {
+            case .result:
+                completion(result)
+            case .main:
+                main.addOperation { completion(result) }
+            case .current:
+                current.addOperation { completion(result) }
+            }
         }
     }
-    
+
     @discardableResult
-    func onSuccess(completion: @escaping (T) -> Void) -> Progress? {
-        return self.start { result in
+    func onSuccess(queue: CompletionQueue = .result, completion: @escaping (T) -> Void) -> Progress? {
+        return self.start(queue: queue) { result in
             if case .success(let value) = result {
                 completion(value)
             }
         }
     }
-    
+
     @discardableResult
-    func onError(completion: @escaping (Error) -> Void)-> Progress? {
-        return self.start { result in
+    func onError(queue: CompletionQueue = .result, completion: @escaping (Error) -> Void)-> Progress? {
+        return self.start(queue: queue) { result in
             if case .error(let error) = result {
                 completion(error)
             }
         }
     }
-    
-    func map<U>(f: @escaping (T) -> U) -> Future<U> {
+
+    func map<U>(queue: CompletionQueue = .result, f: @escaping (T) -> U) -> Future<U> {
         return Future<U> ({ completion in
-            return self.start { result in
+            return self.start(queue: queue) { result in
                 switch result {
                 case .success(let value): completion(Result.success(f(value)))
                 case .error(let error): completion(Result.error(error))
@@ -74,9 +91,9 @@ public extension Future {
         })
     }
 
-    func mapError(f: @escaping (Error) -> Error) -> Future<T> {
+    func mapError(queue: CompletionQueue = .result, f: @escaping (Error) -> Error) -> Future<T> {
         return Future ({ completion in
-            return self.start { result in
+            return self.start(queue: queue) { result in
                 switch result {
                 case .success(let value): completion(Result.success(value))
                 case .error(let error): completion(Result.error(f(error)))
@@ -84,10 +101,10 @@ public extension Future {
             }
         })
     }
-    
-    func flatMap<U>(f: @escaping (T) -> Future<U>) -> Future<U> {
+
+    func flatMap<U>(queue: CompletionQueue = .result, f: @escaping (T) -> Future<U>) -> Future<U> {
         return Future<U>({ completion in
-            return self.start { firstFutureResult in
+            return self.start(queue: queue) { firstFutureResult in
                 switch firstFutureResult {
                 case .success(let value): f(value).start(completion: completion)
                 case .error(let error): completion(Result.error(error))
