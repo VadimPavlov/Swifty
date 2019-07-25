daf2b8771b2d87f436a46c6c19de5010f20165ab
Vadim Pavlov <vadim.pavlov@tbwamobile.com>
2019-07-25 15:06:18 +0300

    objc notation for UIViewController

--- Source/ios/UIKit/UIViewController.swift
+++ Source/ios/UIKit/UIViewController.swift
@@ -12,12 +12,12 @@ extension UIViewController {
 
 // MARK: - Alerts
 extension UIViewController {
-    public func showAlert(error: NSError) {
+    @objc open func showAlert(error: NSError) {
         let alert = UIAlertController(error: error)
         self.present(alert, animated: true, completion: nil)
     }
     
-    public func showAlert(errorMessage: String) {
+    @objc open func showAlert(errorMessage: String) {
         let alert = UIAlertController(errorMessage: errorMessage)
         self.present(alert, animated: true, completion: nil)
     }
